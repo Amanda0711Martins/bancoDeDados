bd330ce436916838173d2eb4bf943814fb2caf25
Amanda Martins <amanda_nelva07@hotmail.com>
2024-08-15 20:13:24 -0300

    first commit

---
+++ src/test/java/com/TPShowDoMilhao/bancoDeDados/BancoDeDadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TPShowDoMilhao.bancoDeDados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoDeDadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
